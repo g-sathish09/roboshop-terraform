18f7812a44266106b38bf6cc0fc48fc76239ca16
gunasekaransathish <sathish.gunasekaran@gmail.com>
2025-03-17 19:57:51 +0530

    Added argocd installation step

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -56,3 +56,14 @@ resource "aws_eks_node_group" "main" {
     min_size     = each.value["min_size"]
   }
 }
+
+## ArgoCD Setup
+resource "null_resource" "argocd" {
+  provisioner "local-exec" {
+    command = <<EOF
+ kubectl create namespace argocd
+ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+ kubectl patch svc argocd-server -p '{"spec": {"ports": [{"port": 443,"targetPort": 8080,"name": "https"},{"port": 80,"targetPort": 8080,"name": "http"}],"type": "LoadBalancer"}}' -n argocd
+ EOF
+  }
+}
