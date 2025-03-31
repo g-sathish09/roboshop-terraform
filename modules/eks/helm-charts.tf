91d115664e2ad2f55066f5733959049121a51829
gunasekaransathish <sathish.gunasekaran@gmail.com>
2025-03-31 13:32:14 +0530

    Added argocd and nginx helm also provider

---
+++ modules/eks/helm-charts.tf
@@ -0,0 +1,41 @@
+resource "null_resource" "kube-bootstrap" {
+  provisioner "local-exec" {
+    command =<<EOF
+aws eks update-kubeconfig  --name ${var.env}-eks
+kubectl create ns devops
+EOF
+  }
+}
+
+resource "helm_release" "nginx-ingress" {
+  depends_on = [null_resource.kube-bootstrap]
+  chart = "oci://ghcr.io/nginxinc/charts/nginx-ingress"
+  name  = "nginx-ingress"
+  namespace = "devops"
+  wait       = true
+
+  values = [
+    file("${path.module}/helm-config/nginx-ingress.yml")
+  ]
+}
+
+## ArgoCD Setup
+resource "helm_release" "argocd" {
+  depends_on = [null_resource.kube-bootstrap]
+
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  wait             = false
+
+  set {
+    name  = "global.domain"
+    value = "argocd-${var.env}.harsharoboticshop.online"
+  }
+
+  values = [
+    file("${path.module}/helm-config/argocd.yml")
+  ]
+}
