a43c8486942d195bc89f6731c8f43cb6a07d6e09
gunasekaransathish <sathish.gunasekaran@gmail.com>
2025-05-02 15:11:37 +0530

    Added IGW and Natgateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,14 +5,68 @@ resource "aws_vpc" "main" {
   }
 }
 
-
-resource "aws_subnet" "subnet" {
-  for_each   = var.subnets
-  vpc_id     = aws_vpc.main.id
-  cidr_block = each.value["cidr_block"]
+resource "aws_subnet" "main" {
+  for_each          = var.subnets
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value["cidr_block"]
+  availability_zone = each.value["az"]
   tags = {
     Name = "${var.env}-${each.key}"
   }
+}
+
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id   = aws_vpc.main.id
+  tags = {
+    Name = "${var.env}-${each.key}"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  for_each       = var.subnets
+  subnet_id      = aws_subnet.main[each.key].id
+  route_table_id = aws_route_table.main[each.key].id
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+resource "aws_eip" "igw" {
+  count  = length(local.az)
+  domain = "vpc"
+  tags = {
+    Name = "${var.env}-ngw"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  count         = length(local.az)
+  allocation_id = aws_eip.igw[count.index].id
+  subnet_id     = aws_subnet.main[local.igw_enabled_subnets[count.index]].id
+
+  tags = {
+    Name = "${var.env}-ngw"
+  }
+}
+
+resource "aws_route" "igw" {
+  count                  = length(local.igw_enabled_subnets)
+  route_table_id         = aws_route_table.main[local.igw_enabled_subnets[count.index]].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route" "ngw" {
+  count                  = length(local.ngw_enabled_subnets)
+  route_table_id         = aws_route_table.main[local.ngw_enabled_subnets[count.index]].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.main.*.id, count.index)
 }
 
 resource "aws_vpc_peering_connection" "main" {
@@ -22,7 +76,8 @@ resource "aws_vpc_peering_connection" "main" {
 }
 
 resource "aws_route" "main" {
-  route_table_id            = aws_vpc.main.default_route_table_id
+  for_each                  = var.subnets
+  route_table_id            = aws_route_table.main[each.key].id
   destination_cidr_block    = var.default_vpc["cidr"]
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
@@ -31,31 +86,41 @@ resource "aws_route" "default-vpc-route-table" {
   route_table_id            = var.default_vpc["route_table"]
   destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-
 }
 
 
-resource "aws_security_group" "test" {
-  name = "test"
-  vpc_id = aws_vpc.main.id
 
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+# resource "aws_security_group" "test" {
+#   name = "test"
+#   vpc_id = aws_vpc.main.id
+#
+#   ingress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+#
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+#
+# }
+#
+# resource "aws_instance" "test" {
+#   ami           = "ami-0fe5f70ea69ebc416"
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = [aws_security_group.test.id]
+#   subnet_id = aws_subnet.main["private"].id
+# }
+#
 
-}
 
 
 resource "aws_instance" "test" {
