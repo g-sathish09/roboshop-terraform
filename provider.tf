a0abb0b798ba1ff1e470e6b4189c99e301d9da23
gunasekaransathish <sathish.gunasekaran@gmail.com>
2025-06-04 00:40:58 +0530

    Added subnet

--- provider.tf
+++ provider.tf
@@ -4,15 +4,7 @@ provider "vault" {
   skip_tls_verify = true
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
-  }
+provider "kubernetes" {
+  config_path = "~/.kube/config"
 }
 
