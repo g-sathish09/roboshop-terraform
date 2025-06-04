9b7c8603386ccc362f2130fd861c5dda18f7381c
gunasekaransathish <sathish.gunasekaran@gmail.com>
2025-06-04 12:38:03 +0530

    Added subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -83,11 +83,11 @@ resource "aws_route" "main" {
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
 
-# resource "aws_route" "default-vpc-route-table" {
-#   route_table_id            = var.default_vpc["route_table"]
-#   destination_cidr_block    = var.cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-# }
+resource "aws_route" "default-vpc-route-table" {
+  route_table_id            = var.default_vpc["route_table"]
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
 
 
 
