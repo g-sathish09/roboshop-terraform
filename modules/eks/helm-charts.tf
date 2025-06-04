656c3e76eac875972650bb5f4227eb95b30c1bd1
gunasekaransathish <sathish.gunasekaran@gmail.com>
2025-06-04 12:43:28 +0530

    Added subnet

--- modules/eks/helm-charts.tf
+++ modules/eks/helm-charts.tf
@@ -84,37 +84,37 @@ resource "helm_release" "external-secrets" {
   wait             = true
 }
 
-resource "null_resource" "external-secret" {
-  depends_on = [helm_release.external-secrets]
+# resource "null_resource" "external-secret" {
+#   depends_on = [helm_release.external-secrets]
 
-  provisioner "local-exec" {
-    command = <<EOF
-kubectl apply -f - <<EOK
-apiVersion: v1
-kind: Secret
-metadata:
-  name: vault-token
-data:
-  token: aHZzLm85R1NRbnpXNFNMTmhZSWE4aVllWlNuVQ==
----
-apiVersion: external-secrets.io/v1beta1
-kind: ClusterSecretStore
-metadata:
-  name: vault-backend
-spec:
-  provider:
-    vault:
-      server: "http://vault-internal.harsharoboticshop.online:8200"
-      path: "roboshop-${var.env}"
-      version: "v2"
-      auth:
-        tokenSecretRef:
-          name: "vault-token"
-          key: "token"
-EOK
-EOF
-  }
-}
+#   provisioner "local-exec" {
+#     command = <<EOF
+# kubectl apply -f - <<EOK
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: vault-token
+# data:
+#   token: aHZzLm85R1NRbnpXNFNMTmhZSWE4aVllWlNuVQ==
+# ---
+# apiVersion: external-secrets.io/v1beta1
+# kind: ClusterSecretStore
+# metadata:
+#   name: vault-backend
+# spec:
+#   provider:
+#     vault:
+#       server: "http://vault-internal.harsharoboticshop.online:8200"
+#       path: "roboshop-${var.env}"
+#       version: "v2"
+#       auth:
+#         tokenSecretRef:
+#           name: "vault-token"
+#           key: "token"
+# EOK
+# EOF
+#   }
+# }
 
 
 ## Filebeat Helm Chart
